e232a7a880bc4ce1e45e43ffbb766e78c1d28bc2
BooleanAutocrad <chiragtheonly1@gmail.com>
2024-03-29 02:18:47 +0530

    yo

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -21,13 +21,11 @@ const UserProfile = ({params}) => {
     }, [params.id]);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <Profile
         name={userName}
         desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
         data={userPosts}
       />
-    </Suspense>
   )
 }
 
